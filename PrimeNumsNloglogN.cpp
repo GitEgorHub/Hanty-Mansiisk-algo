8501c6639c8cc1c66f8df9004b0a71c5c71dde99
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2022-11-13 13:02:49 +0300

    Create PrimeNumsNloglogN.cpp

---
+++ PrimeNumsNloglogN.cpp
@@ -0,0 +1,25 @@
+#include <bits/extc++.h>
+#define int int64_t
+
+using namespace std;
+
+int32_t main() {
+    int n;
+    cin >> n;
+    bool prime[n + 1];
+    for (int i = 2; i <= n; i++)
+        prime[i] = true;
+    prime[0] = prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (prime[i]) {
+            cout << i << ' ';
+        }
+    }
+}
